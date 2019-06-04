a0cc87cec8dad1d23672e81fa3851613edcd1627
Georgi Georgiev <georgiwess@gmail.com>
2019-06-04 09:46:08 +0300

    Add extra validation, add time picker

--- extension/contentScripts/takeaway-injected.js
+++ extension/contentScripts/takeaway-injected.js
@@ -42,31 +42,51 @@ const newDropdown = document.createElement('div');
 newDropdown.innerHTML = newDropdownContent;
 const cartbuttonElement = document.getElementsByClassName('cartbutton')[0];
 const menuCartFixedElement = document.getElementsByClassName('menu-cart-fixed')[0];
-if (typeof menuCartFixedElement !== 'undefined') {
+if (menuCartFixedElement) {
     menuCartFixedElement.insertBefore(newDropdown, cartbuttonElement);
 }
 
 const newTick42EatsButtonContent = `
 <section class="cartbutton">
-	<a href="#" class="cartbutton-button">New order (Tick42 Eats)</a>
+	<a id="new-tick42-eats" class="cartbutton-button"><input id="time" type="time" value="12:30" required>New order (Tick42 Eats)</a>
 </section>`;
 const newTick42EatsButton = document.createElement('div');
 newTick42EatsButton.innerHTML = newTick42EatsButtonContent;
-if (typeof menuCartFixedElement !== 'undefined') {
+if (menuCartFixedElement) {
     menuCartFixedElement.insertBefore(newTick42EatsButton, cartbuttonElement);
 }
 
-document.getElementsByClassName('nav-button')[0].addEventListener('click', function () {
-    this.parentNode.parentNode.classList.toggle('closed');
-}, false);
+const navButtonElement = document.getElementsByClassName('nav-button')[0];
+if (navButtonElement) {
+    navButtonElement.addEventListener('click', function () {
+        this.parentNode.parentNode.classList.toggle('closed');
+    }, false);
+}
+
+const timeElement = document.getElementById('time');
+if (timeElement) {
+    timeElement.addEventListener('click', (event) => {
+        event.stopPropagation();
+    }, false);
+}
+
+const newTick42EatsElement = document.getElementById('new-tick42-eats');
+if (newTick42EatsElement) {
+    newTick42EatsElement.addEventListener('click', () => {
+        const timeElement = document.getElementById('time');
+        const newTick42EatsOrderTime = timeElement.value;
+    }, false);
+}
 
 const tick42OrderElements = document.getElementsByClassName('tick42-order');
-Array.from(tick42OrderElements).forEach((tick42OrderElement) => {
-    tick42OrderElement.addEventListener('click', function () {
-        tick42OrderElement.parentNode.parentNode.classList.toggle('closed');
-        console.log(this.innerHTML);
-    }, false);
-});
+if (tick42OrderElements) {
+    Array.from(tick42OrderElements).forEach((tick42OrderElement) => {
+        tick42OrderElement.addEventListener('click', function () {
+            tick42OrderElement.parentNode.parentNode.classList.toggle('closed');
+            console.log(this.innerHTML);
+        }, false);
+    });
+}
 
 const getCurrentRestaurant = (currUrl = window.location.href) => {
     const urlElements = currUrl.split("/");
