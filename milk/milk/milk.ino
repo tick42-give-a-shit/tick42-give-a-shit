70629d2e2b9d37410371bdb3b1ae68692d21e052
Velko Nikolov <velko.nikolov@tick42.com>
2019-06-04 14:59:19 +0300

    milk alpha
    [master]

---
+++ milk/milk/milk.ino
@@ -0,0 +1,198 @@
+#include <ESP8266WiFi.h>
+#include <WebSocketClient.h>
+#include <ArduinoJson.h>
+
+const char WIFI_SSID[] = "Pirinsoft";
+const char WIFI_PSK[] = "+rqcP3_nnhSH]Yr%";
+
+const int LED_PIN = 5;
+const uint8_t GPIO0 = 0;
+
+WiFiClient client;
+WebSocketClient webSocketClient;
+
+
+void connectWiFi() {
+
+  Serial.print("connecting");
+  byte led_status = 0;
+
+  // Set WiFi mode to station (client)
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(WIFI_SSID, WIFI_PSK);
+
+  // Blink LED while we wait for WiFi connection
+  while ( WiFi.status() != WL_CONNECTED ) {
+    digitalWrite(LED_PIN, led_status);
+    led_status ^= 0x01;
+    delay(100);
+  }
+
+  // Turn LED off when we are connected
+  digitalWrite(LED_PIN, HIGH);
+  Serial.print("done");
+
+}
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(GPIO0, INPUT);
+  
+}
+
+void morse(String str) {
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+  for (int ii = 0; ii < str.length(); ++ii) {
+    if (str.charAt(ii) == '.') {
+      digitalWrite(LED_PIN, HIGH);
+      delay(300);
+      digitalWrite(LED_PIN, LOW);
+      delay(500);
+    } else if (str.charAt(ii) == '-') {
+      digitalWrite(LED_PIN, HIGH);
+      delay(1000);
+      digitalWrite(LED_PIN, LOW);
+      delay(500);
+    } else {
+      delay(1000);
+    }
+  }
+}
+
+String getJsonField(String json, String field) {
+  char* where = strstr(strstr(strstr(json.c_str(), field.c_str()), "\"") + 1, "\"") + 1;
+  char dest[200];
+  int len = strstr(where, "\"") - where;
+  strncpy(dest, where, len);
+  dest[len] = '\0';
+  String value = String(dest);
+  return String(value);
+}
+
+void loop() {
+
+  int debug = 0;
+  char* host = "35.242.253.103";
+  if (debug) {
+    host = "192.168.1.201";
+  }
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+  
+  if (WiFi.status() != WL_CONNECTED) {
+    return;
+  }
+
+  if (!client.connect(host, 5000)) {
+    morse("...  ...  ...  ");
+    return;
+  }
+
+  webSocketClient.path = "/gw";
+  webSocketClient.host = host;
+  if (!webSocketClient.handshake(client)) {
+    morse("..  ..  ..  ");
+    return;
+  }
+  
+  morse(".  .  .  ");
+
+  String data;
+  /*
+  JSON.stringify(
+      {
+          type: "hello",
+          request_id: "0",
+          domain: "global",
+          identity: {
+              application: "tick42-got-sensor"
+          },
+          authentication: {
+              method: "secret",
+              login: "tick42_got",
+              secret: "secret"
+          },
+      });
+  */
+
+  data = String("{\"type\":\"hello\",\"request_id\":\"0\",\"domain\":\"global\",\"identity\":{\"application\":\"tick42-got-sensor\"},\"authentication\":{\"method\":\"secret\",\"login\":\"tick42_got\",\"secret\":\"secret\"}}");
+  webSocketClient.sendData(data);
+  do {
+    delay(5000);
+    webSocketClient.getData(data);
+    morse("...  ...  ...  ");
+  }
+  while (client.connected() && !data.length());
+
+  String peerId = getJsonField(data, "peer_id");
+
+  /*
+  JSON.stringify(
+      {
+        "domain": "global",
+        "type": "create-context",
+        "peer_id": peerId,
+        "request_id": "1",
+        "name": "test",
+        "lifetime": "retained"
+    });
+  */
+  data = String("{\"domain\":\"global\",\"type\":\"create-context\",\"peer_id\":\"#peerId#\",\"request_id\":\"1\",\"name\":\"GOT_Extension\",\"lifetime\":\"retained\"}");
+  data.replace("#peerId#", peerId);
+  webSocketClient.sendData(data);
+  do {
+    delay(5000);
+    webSocketClient.getData(data);
+    morse("...  ...  ...  ");
+  } while (client.connected() && !data.length());
+
+  String contextId = getJsonField(data, "context_id");
+  
+  /*
+  JSON.stringify(
+    {
+        "domain": "global",
+        "type": "update-context",
+        "request_id": "3",
+        "peer_id": "peerId",
+        "context_id": "contextId",
+        "delta": { state: "true" }
+    });
+  */
+  int state = 1;
+  while (client.connected()) {
+    data = 
+      "{\"domain\":\"global\",\"type\":\"update-context\",\"request_id\":\"3\",\"peer_id\":\"#peerId#\",\"context_id\":\"#contextId#\",\"delta\":{\"updated\":{\"milk\":#state#}}}";
+    data.replace("#peerId#", peerId);
+    data.replace("#contextId#", contextId);
+  
+    byte pressed = (digitalRead(GPIO0) == LOW);
+
+    if (pressed) {
+      state = !state;
+    }
+
+    if (debug) {
+      webSocketClient.sendData(String(state));
+    }
+    
+    if (pressed) {
+      data.replace("#state#", state ? "true" : "false");
+
+      digitalWrite(LED_PIN, !state ? HIGH : LOW);
+
+      webSocketClient.sendData(data);
+
+      // drain websocket
+      webSocketClient.getData(data);
+    }
+    delay(3000);
+  }
+
+}
