c037b9119d4eda453b2721125fa9cad554dd05a7
Velko Nikolov <velko.nikolov@tick42.com>
2019-06-04 14:26:22 +0300

    milk/milk-old-test.ino
    [master]

---
+++ milk/milk-old-test.ino
@@ -0,0 +1,37 @@
+static const uint8_t D1 = 0;
+const int LED_PIN = 5;
+
+void setup() {
+  pinMode(D1, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  for (int ii = 0; ii < 10; ++ ii) {
+         digitalWrite(LED_PIN, HIGH);
+     delay(200);
+     digitalWrite(LED_PIN, LOW);
+     delay(200);
+  }
+}
+
+void loop() {
+  byte valor = digitalRead(D1); 
+
+  if(valor == HIGH) {         
+     digitalWrite(LED_PIN, LOW);
+     delay(1000);
+     digitalWrite(LED_PIN, HIGH);
+     delay(1000);
+  } else {
+     digitalWrite(LED_PIN, HIGH);
+     delay(200);
+     digitalWrite(LED_PIN, LOW);
+     delay(200);
+     digitalWrite(LED_PIN, HIGH);
+     delay(200);
+     digitalWrite(LED_PIN, LOW);
+     delay(200);
+     digitalWrite(LED_PIN, HIGH);
+     delay(1000);
+  }
+
+}
